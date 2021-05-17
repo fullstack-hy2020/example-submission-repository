871a2a539d5f8b7851f7d7408a59a58859d121ba
Saku <>
2021-05-17 23:16:31 +0900

    ex1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,32 +2,34 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <h1>{course}</h1>
       <p>
-        {part1.name} {part1.exercises}
+        {parts[0].name} {parts[0].exercises}
       </p>
       <p>
-        {part2.name} {part2.exercises}
+        {parts[1].name} {parts[1].exercises}
       </p>
       <p>
-        {part3.name} {part3.exercises}
+        {parts[2].name} {parts[2].exercises}
       </p>
-      <p>Number of exercises {part1.name + part2.name + part3.name}</p>
+      <p>Number of exercises {parts[0].name + parts[1].name + parts[2].name}</p>
     </div>
   )
 }
