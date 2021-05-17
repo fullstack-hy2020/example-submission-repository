4a98f0c1e4e9fc930678903f795d34fd4f2aeab6
Saku <>
2021-05-17 23:16:12 +0900

    ex1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,30 +2,40 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <h1>{course}</h1>
       <p>
-        {part1} {exercises1}
+        {part1.name} {part1.exercises}
       </p>
       <p>
-        {part2} {exercises2}
+        {part2.name} {part2.exercises}
       </p>
       <p>
-        {part3} {exercises3}
+        {part3.name} {part3.exercises}
       </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>Number of exercises {part1.name + part2.name + part3.name}</p>
     </div>
   )
 }
 
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
+}
 
 
 export default App
