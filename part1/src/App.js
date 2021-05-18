cafdd56e7bf6357c7326e5370797814ec5d8bf82
Saku <>
2021-05-18 12:34:01 +0900

    ex1.8

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,9 +6,36 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const getAll = () => good + neutral + bad
+  const getAverage = () => {
+    const all = getAll()
+    return all === 0 ? 0 : (good - bad) / getAll()
+  }
+  const getPositive = () => {
+    const all = getAll()
+    return all === 0 ? 0 : (good) / getAll()
+  }
+  return (
+    <div>
+      good {good}<br />
+      neutral {neutral}<br />
+      bad {bad}<br />
+      all {getAll()}<br />
+      average {getAverage()}<br />
+      positive {getPositive()} %<br />
     </div>
   )
 }
