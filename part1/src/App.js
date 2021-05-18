2b2515e127e7dbfe210840ee4bf184b646617272
Saku <>
2021-05-18 12:17:11 +0900

    ex1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,40 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <h1>{course.name}</h1>
-      <p>
-        {course.parts[0].name} {course.parts[0].exercises}
-      </p>
-      <p>
-        {course.parts[1].name} {course.parts[1].exercises}
-      </p>
-      <p>
-        {course.parts[2].name} {course.parts[2].exercises}
-      </p>
-      <p>Number of exercises {course.parts[0].name + course.parts[1].name + course.parts[2].name}</p>
+      code here
     </div>
   )
 }
-
 
 export default App
