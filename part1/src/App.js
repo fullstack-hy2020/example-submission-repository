0d083945350b627089e33dcdbc22ffdef7bf25c5
Saku <>
2021-05-18 12:56:20 +0900

    ex1.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,56 +1,24 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const getRandomInt = (max) => Math.floor(Math.random() * max)
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      {anecdotes[selected]}<br/>
+      <button onClick={() => setSelected(getRandomInt(6))}>next anecdote</button>
     </div>
   )
 }
-
-const Statistics = ({good, neutral, bad}) => {
-  const getAll = () => good + neutral + bad
-  const getAverage = () => {
-    const all = getAll()
-    return all === 0 ? 0 : (good - bad) / getAll()
-  }
-  const getPositive = () => {
-    const all = getAll()
-    return all === 0 ? 0 : (good) / getAll()
-  }
-  if (getAll() === 0) {
-    return (<div>No feedback given</div>)
-  } else {
-    return (
-      <table>
-        <tbody>
-          <Statistic text="good" value={good} />
-          <Statistic text="neutral" value={neutral} />
-          <Statistic text="bad" value={bad} />
-          <Statistic text="all" value={getAll()} />
-          <Statistic text="average" value={getAverage()} />
-          <Statistic text="positive" value={getPositive()} suffix={" %"} />
-        </tbody>
-      </table>
-    )
-  }
-}
-
-const Statistic = ({ text, value, suffix}) =>
-  <tr>
-    <td>{text}</td>
-    <td>{value}{suffix}</td>
-  </tr>
-
-
 
 export default App
