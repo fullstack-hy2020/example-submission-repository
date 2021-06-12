be252d140e8084e5d82d1d31672dc43b99b73070
Saku <>
2021-06-12 15:22:28 +0900

    part 2-1 again

--- part2/src/components/Course.js
+++ part2/src/components/Course.js
@@ -1,20 +1,16 @@
 import React from 'react'
 
-const Course = ({ course }) => {
-    return (
-      <div>
-          <h1>{course.name}</h1>
-          <p>
-              {course.parts[0].name} {course.parts[0].exercises}
-          </p>
-          <p>
-              {course.parts[1].name} {course.parts[1].exercises}
-          </p>
-          <p>
-              {course.parts[2].name} {course.parts[2].exercises}
-          </p>
-      </div>
-    )
+const Course = ({course}) => {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      {course.parts.map(part =>
+        <p>
+          {part.name} {part.exercises}
+        </p>
+      )}
+    </div>
+  )
 }
 
 
