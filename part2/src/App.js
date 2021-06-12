8cf12ebbf84fdf4b366bba942a0b0c39a3263ee8
Saku <>
2021-06-12 14:55:30 +0900

    before part2-1

---
+++ part2/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
