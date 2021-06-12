4c807533e4b1da02ceecbd7a44389f7b76d85454
Saku <>
2021-06-13 02:31:47 +0900

    before part2-12

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,31 +1,11 @@
-import React, {useState} from 'react'
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: '040-123456'},
-    {name: 'Ada Lovelace', number: '39-44-5323523'},
-    {name: 'Dan Abramov', number: '12-43-234345'},
-    {name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ])
-
-  const [searchQuery, setSearchQuery] = useState('')
-
-  return (
-
-    <div>
-      <h2>Phonebook</h2>
-      <Filter query={searchQuery} setSearchQuery={setSearchQuery}/>
-
-      <h3>Add a new</h3>
-      <PersonForm persons={persons} setPersons={setPersons}/>
-
-      <h3>Numbers</h3>
-      <Persons persons={persons} searchQuery={searchQuery}/>
-    </div>
-  )
+  
 }
 
 export default App
--- part2/src/index.js
+++ part2/src/index.js
@@ -1,28 +1,8 @@
 import ReactDOM from 'react-dom'
 import App from './App'
-
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
+import axios from 'axios'
 
 ReactDOM.render(
-  <App notes={notes}></App>,
+  <App></App>,
   document.getElementById('root')
 )
