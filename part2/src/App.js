e16f0c8ac19bb3816668ae351549e08dbfda6981
Saku <>
2021-06-13 02:37:23 +0900

    part 2-12

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,11 +1,19 @@
-import Persons from "./components/Persons";
-import PersonForm from "./components/PersonForm";
-import Filter from "./components/Filter";
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  
+  const [persons, setPersons] = useState([{}])
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons")
+      .then(response => {
+        console.log(response)
+        setPersons(response.data)
+      })
+  }, [])
+  return (
+    <div>hello</div>
+  )
+
 }
 
 export default App
