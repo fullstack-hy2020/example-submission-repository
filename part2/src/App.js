452cb5d209b7e852752334a0f58c266d14697409
Saku <>
2021-06-12 16:47:49 +0900

    part 2-6

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,54 +1,37 @@
-import React, {useState} from 'react'
-import Note from './components/Note'
+import React, { useState } from 'react'
+import Person from './components/Person'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  )
-  const [showAll, setShowAll] = useState(true)
-  const notesToShow = showAll
-    ? notes
-    : notes.filter(note => note.important === true)
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
 
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
   }
 
-  const addNote = (event) => {
+  const addName = (event) => {
     event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+    const newPerson = {
+      name: newName
     }
-
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    setPersons(persons.concat(newPerson))
   }
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote}
-               onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
       </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <Person person={person}/>)}
     </div>
   )
 }
---
+++ part2/src/components/Person.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Person = ({ person }) => {
+  return (
+    <div>{person.name}</div>
+  )
+}
+
+export default Person
