f348ad0ed262b22acb1cb266b3f282c89fba4ade
Saku <>
2021-06-14 19:23:50 +0900

    part 2-12

--- part2/src/App.js
+++ part2/src/App.js
@@ -2,16 +2,48 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([{}])
+  const [countries, setCountries] = useState([])
+  const [query, setQuery] = useState("")
+
+  const handleQuery = (event) => {
+    setQuery(event.target.value)
+  }
+
+  const filterResult = (countries) => {
+    const filteredResult = countries.filter(country => !query || country.name.toLowerCase().includes(query.toLowerCase()))
+    if (filteredResult.length > 10) {
+      return ("Too many matches, specify another filter")
+    } else if (filteredResult.length <= 0) {
+      return ("No matches, specify another filter")
+    } else if (filteredResult.length === 1) {
+      const country = filteredResult[0]
+      return (
+        <div>
+          <h2>{country.name}</h2>
+          <div>capital {country.capital}</div>
+          <div>population {country.population}</div>
+          <h3>languages</h3>
+          <ul>{country.languages.map(language => <li>{language.name}</li>)}</ul>
+          <img src={country.flag} height={200}/>
+        </div>
+      )
+    } else {
+      return (filteredResult.map(country => <div key={country.name}>{country.name}</div>))
+    }
+  }
+
   useEffect(() => {
-    axios.get("http://localhost:3001/persons")
+    axios.get("https://restcountries.eu/rest/v2/all")
       .then(response => {
-        console.log(response)
-        setPersons(response.data)
+        setCountries(response.data)
       })
   }, [])
+
   return (
-    <div>hello</div>
+    <div>
+      <div>find countries<input onChange={handleQuery} value={query}/></div>
+      {filterResult(countries)}
+    </div>
   )
 
 }
