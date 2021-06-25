0e6a29e177782fb74cec4c8a6256788024315d65
Saku <>
2021-06-25 23:01:03 +0900

    part3-4

--- part3/index.js
+++ part3/index.js
@@ -48,36 +48,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  const person = persons.find(person => person.id === id)
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-  notes = notes.concat(note)
-  response.json(note)
-})
 
 const PORT = 3001
 app.listen(PORT, () => {
