178d883850060577e0c523cad9214cface3ba919
Saku <>
2021-06-26 16:40:40 +0900

    part 3-7, 3-8

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
