9dd58e6a2551748796f072b2f7f39c535d111e07
user2695 <zy.khan37@gmail.com>
2022-09-03 14:54:27 +0530

    first commit

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,19 @@
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
+    </div>
+  );
+};
+
+export default App;
---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Content() {
+  return <div>Content</div>;
+}
+
+export default Content;
