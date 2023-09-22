2d01c8355964b6a0f5765bcaf6c43905d61547cf
Joseph Marie Alba <albajosephmarie@gmail.com>
2023-09-22 04:44:19 +0200

    exercises-1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,6 +16,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {good+neutral+bad}</div>
+      <div>average {(good-bad)/(good+neutral+bad)}</div>
+      <div>positive {good/(good+neutral+bad)*100}%</div>
     </div>
   )
 }
