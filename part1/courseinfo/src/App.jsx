97f6de7559f7d6c4a528ef50ec3170950e527447
Joseph Marie Alba <albajosephmarie@gmail.com>
2023-09-22 02:29:24 +0200

    exercises-1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.name}</h1>;
 };
 
 const Part = (props) => {
@@ -31,29 +31,43 @@ const Total = (props) => {
   );
 };
 
+const Course = (props) => {
+  return (
+    <>
+      <Header name={props.course.name} />
+      <Content parts={props.course.parts} />
+      <Total
+        total={
+          props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises
+        }
+      />
+    </>
+  );
+};
+
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total
-        total={parts[0].exercises + parts[1].exercises + parts[2].exercises}
-      />
+      <Course course={course} />
     </div>
   );
 };
